68353feecb01ad9ef59586d9b7ab7628cbfc2b3d
Muccy <muccymac@gmail.com>
2012-04-30 15:45:07 +0200

    First Commit

---
+++ MUKObjectCache/Unit Tests/MUKObjectCacheTests.h
@@ -0,0 +1,13 @@
+//
+//  MUKObjectCacheTests.h
+//  MUKObjectCacheTests
+//
+//  Created by Marco Muccinelli on 30/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MUKObjectCacheTests : SenTestCase
+
+@end
---
+++ MUKObjectCache/Unit Tests/MUKObjectCacheTests.m
@@ -0,0 +1,32 @@
+//
+//  MUKObjectCacheTests.m
+//  MUKObjectCacheTests
+//
+//  Created by Marco Muccinelli on 30/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MUKObjectCacheTests.h"
+
+@implementation MUKObjectCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MUKObjectCacheTests");
+}
+
+@end
